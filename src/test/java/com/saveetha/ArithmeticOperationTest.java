c0e9b12145357139d480764fac3971eb0a0a6b56
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-07-26 20:36:16 +0530

    ADD. Added tests for subtraction.

--- src/test/java/com/saveetha/ArithmeticOperationTest.java
+++ src/test/java/com/saveetha/ArithmeticOperationTest.java
@@ -34,6 +34,64 @@ public class ArithmeticOperationTest {
 
             assertEquals(minusFive, actualSum);
         }
+
+        @Test
+        void toReturnTwoWhenZeroIsAddedWithTwo() {
+
+            ArithmeticOperation arithmeticOperation = new ArithmeticOperation();
+            int zero = 0, two = 2;
+
+            int actualSum = arithmeticOperation.add(two, zero);
+
+            assertEquals(two, actualSum);
+        }
+    }
+
+    @Nested
+    class Subtraction {
+        @Test
+        void toReturnTwoWhenFiveIsSubtractedWithThree() {
+
+            ArithmeticOperation arithmeticOperation = new ArithmeticOperation();
+            int two = 2, three = 3, five = 5;
+
+            int actualSum = arithmeticOperation.sub(five, three);
+
+            assertEquals(two, actualSum);
+        }
+
+        @Test
+        void toReturnMinusNineWhenMinusSevenIsSubtractedWithTwo() {
+
+            ArithmeticOperation arithmeticOperation = new ArithmeticOperation();
+            int minusSeven = -7, minusNine = -9, two = 2;
+
+            int actualSum = arithmeticOperation.sub(minusSeven, two);
+
+            assertEquals(minusNine, actualSum);
+        }
+
+        @Test
+        void toReturnMinusTwoWhenZeroIsSubtractedWithTwo() {
+
+            ArithmeticOperation arithmeticOperation = new ArithmeticOperation();
+            int zero = 0, two = 2, minusTwo = -2;
+
+            int actualSum = arithmeticOperation.sub(zero, two);
+
+            assertEquals(minusTwo, actualSum);
+        }
+
+        @Test
+        void toReturnZeroWhenZeroIsSubtractedWithZero() {
+
+            ArithmeticOperation arithmeticOperation = new ArithmeticOperation();
+            int zero = 0;
+
+            int actualSum = arithmeticOperation.sub(zero, zero);
+
+            assertEquals(zero, actualSum);
+        }
     }
 
 }
